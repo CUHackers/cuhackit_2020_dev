97ce1de78a2dad10ea872f7b24b74bdb4bb75a63
aru-py <arunorth@gmail.com>
2019-12-15 13:11:43 -0500

    nitial Commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { ScheduleComponent } from './schedule/schedule.component';
+import { PartnersComponent } from './partners/partners.component';
+import { ContactComponent } from './contact/contact.component';
+import { QuotesComponent } from './quotes/quotes.component';
+import { AboutComponent } from './about/about.component';
+import { NavigationComponent } from './navigation/navigation.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    ScheduleComponent,
+    PartnersComponent,
+    ContactComponent,
+    QuotesComponent,
+    AboutComponent,
+    NavigationComponent
   ],
   imports: [
     BrowserModule,
