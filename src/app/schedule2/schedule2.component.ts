f65db60d919cd3dc4b9cf2ad43423d59607100e6
aru-py <arunorth@gmail.com>
2019-12-19 18:29:18 -0500

    Schedule v2

--- src/app/schedule2/schedule2.component.ts
+++ src/app/schedule2/schedule2.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-schedule2',
   templateUrl: './schedule2.component.html',
-  styleUrls: ['./schedule2.component.css']
+  styleUrls: ['../schedule/schedule.component.css']
 })
 export class Schedule2Component implements OnInit {
 
