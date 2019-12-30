fba1ec902b0034948cbba04233a553baeb576aba
aru-py <arunorth@gmail.com>
2019-12-29 20:33:41 -0500

    Schedule Final Update

--- src/app/schedule/schedule.component.ts
+++ src/app/schedule/schedule.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import * as saturday from './Saturday.json';
+import moment from 'moment';
+require('twix')
 
 @Component({
   selector: 'app-schedule',
@@ -9,7 +12,46 @@ export class ScheduleComponent implements OnInit {
 
   constructor() { }
 
+  saturday = (saturday as any).default;
+
   ngOnInit() {
+    var  j = 0;
+
+    this.saturday.forEach((event, i) => {
+
+      let st = moment(event['Start Time'],"LT")
+      let et = moment(event['End Time'],"LT")
+
+      let length = st.twix(et).length("hours");
+      let offset = st.subtract(540,"minutes").hours()
+      if(!length)
+        length++
+
+      if(event['Column'] - 1)
+        event['Color'] = "bg-info"
+      else {
+        event['Color'] = ['bg-secondary','bg-primary'][j%2]
+        j++;
+      }
+
+      event['Length'] = length*45
+      event['Offset'] = offset*45
+
+    })
+
+    for (let i = 9; i <= 21; i+=2) {
+      let t:string;
+      if ( i > 11)
+        t = (i - 12) + ":00 PM"
+      else
+        t = i + ":00 AM"
+      this.clock.push(t);
+    }
   }
+
+  height = 550/7;
+  clock = []
+
+
 
 }
--- src/app/schedule2/schedule2.component.ts
+++ src/app/schedule2/schedule2.component.ts
@@ -1,15 +1,52 @@
 import { Component, OnInit } from '@angular/core';
+import moment from 'moment';
+require('twix')
+import * as sunday from './Sunday.json';
 
 @Component({
   selector: 'app-schedule2',
   templateUrl: './schedule2.component.html',
-  styleUrls: ['../schedule/schedule.component.css']
+  styleUrls: ['./schedule2.component.css']
 })
 export class Schedule2Component implements OnInit {
 
   constructor() { }
 
+  sunday = (sunday as any).default;
+
   ngOnInit() {
-  }
+    var  j = 0;
+
+    this.sunday.forEach((event, i) => {
+
+      let st = moment(event['Start Time'],"LT")
+      let et = moment(event['End Time'],"LT")
+
+      let length = st.twix(et).length("hours");
+      let offset = st.hours()
+      if(!length)
+        length++
+
+      if(event['Column'] - 1)
+        event['Color'] = "bg-info"
+      else {
+        event['Color'] = ['bg-secondary','bg-primary'][j%2]
+        j++;
+      }
+
+      event['Length'] = length*45;
+      event['Offset'] = offset*45;
+
+    })
+
+    let q = moment("12:00 AM", "LT");
+    let b = moment("4:00 PM", "LT");
+    this.clock = q.twix(b).toArray(2, "hours")
+                    .map((a) => a.format("LT"))
+
+}
+  height = 550/7;
+  clock = []
+
 
 }
